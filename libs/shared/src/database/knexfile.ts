e8959874e5bfc03fb8656273fd9cac4c283132fa
Maxime Le Duc <maximeleduc@unknown.home>
2024-10-23 03:04:32 +0200

    Init project

---
+++ libs/shared/src/database/knexfile.ts
@@ -0,0 +1,60 @@
+import { Knex } from 'knex'
+
+const knexConfig: Record<'development' | 'production' | 'test', Knex.Config> = {
+    development: {
+        client: 'postgresql',
+        connection: {
+            host: '127.0.0.1',
+            database: 'discord',
+            user: 'postgres',
+            password: 'secret',
+            port: 5432,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: __dirname + '/migrations/',
+        },
+    },
+    production: {
+        client: 'postgresql',
+        connection: {
+            host: '127.0.0.1',
+            database: 'discord',
+            user: 'postgres',
+            password: 'secret',
+            port: 5432,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: __dirname + '/migrations/',
+        },
+    },
+    test: {
+        client: 'postgresql',
+        connection: {
+            host: '127.0.0.1',
+            database: 'discord',
+            user: 'postgres',
+            password: 'secret',
+            port: 5433,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: __dirname + '/migrations/',
+        },
+    },
+}
+
+export default knexConfig
